b7acf5f95f4af369a563e382670aa71b0957980f
saabalone <abaloneproblems@gmail.com>
2026-09-22 23:49:40 +0200

    KAAH_Test_ph21_v3

--- version.js
+++ version.js
@@ -9,5 +9,5 @@
 //
 // Pas d'import ni d'export (voir moteur/plateau.js).
 const PHASE_KAAH_TEST = 21;
-const VERSION_KAAH_TEST = 2;
+const VERSION_KAAH_TEST = 3;
 const NOM_VERSION_KAAH_TEST = `KAAH_Test_ph${PHASE_KAAH_TEST}_v${VERSION_KAAH_TEST}`;
